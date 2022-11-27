ab62b8ed96b51107ef01fa5b65a20f542e14bb9b
Kah Yeek <kycheok2007@gmail.com>
2022-11-27 16:26:12 +0800

    feat: updated global module content

--- mod_globals_A189479.vb
+++ mod_globals_A189479.vb
@@ -1,3 +1,4 @@
-﻿Module mod_globals_A189479
+﻿Module mod_globals_a189479
+    Public githubLink As String = "https://github.com/KyCooL-pixel/PRJ_ELITEHOOPS_A189479"
     Public connectionString As String = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=DB_ELITEHOOPS_A189479.accdb;Persist Security Info=False;"
 End Module
